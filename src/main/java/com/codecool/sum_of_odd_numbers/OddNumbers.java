57ecba8f7e8a11739a9d475447b08f4c24e83c9a
kisb01 <kisb01@gmail.com>
2021-03-24 15:11:56 +0100

    Clean up

--- src/main/java/com/codecool/sum_of_odd_numbers/OddNumbers.java
+++ src/main/java/com/codecool/sum_of_odd_numbers/OddNumbers.java
@@ -6,7 +6,7 @@ public class OddNumbers {
 
     public static Integer sum(List<Integer> numbers) {
         int sum = 0;
-        for (int i = 0; i < numbers.size(); i++){
+        for (int i = 0; i < numbers.size(); i++) {
             if (Math.abs(numbers.get(i) % 2) == 1) {
                 sum += numbers.get(i);
             }
