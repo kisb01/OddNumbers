3a86440a76c3a3b9d7b0aa03d719e03bd6dbb2ef
kisb01 <kisb01@gmail.com>
2021-03-24 15:09:22 +0100

    Complete Odd Numbers

--- src/main/java/com/codecool/sum_of_odd_numbers/OddNumbers.java
+++ src/main/java/com/codecool/sum_of_odd_numbers/OddNumbers.java
@@ -5,6 +5,12 @@ import java.util.List;
 public class OddNumbers {
 
     public static Integer sum(List<Integer> numbers) {
-        return null;
+        int sum = 0;
+        for (int i = 0; i < numbers.size(); i++){
+            if (Math.abs(numbers.get(i) % 2) == 1) {
+                sum += numbers.get(i);
+            }
+        }
+        return sum;
     }
 }
